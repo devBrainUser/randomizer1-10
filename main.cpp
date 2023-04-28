9cb491b25b2370b7a055cad74e7e193936803263
devBrainUser <PineApplePoor1939@gmail.com>
2023-04-28 13:40:39 +0300

    add comments

--- main.cpp
+++ main.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
-#include <ctime>
+#include <ctime>  // connect new library
 using namespace std;
 
 void main()
 {
 	setlocale(LC_ALL, "rus");
 
-	srand(time(NULL));
+	srand(time(NULL)); 
 
-	int const SIZE = 10;
-	int arr[SIZE];
+	int const SIZE = 10; //setting const variable
+	int arr[SIZE]; // make a massive
 
 	for (int i = 0; i != SIZE; i++)
 	{
-		arr[i] = rand() % 10 + 1;
+		arr[i] = rand() % 10 + 1; // randomizer
 		cout << " ";
 		cout << arr[i];
 	}
